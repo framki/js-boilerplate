207d3ecd19f070142a095cbf3cb5b76d821f0cc2
framki <19401270+framki@users.noreply.github.com>
2018-12-15 15:28:49 +0000

    chore: add eslint no-console rule

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-param-reassign': 'off',
+    'no-restricted-syntax': 'off',
+    'max-len': ['error', 100, 2, { ignoreUrls: true, ignoreComments: true }],
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+};
